389e4c1fccc0c5be5223f6a13dce3be523c9976b
Kamon Ayeva <kamon.ayeva@gmail.com>
2014-01-09 18:50:34 +0100

    Started a browser view needed to import events in the site from a .csv file

--- ageliaco/schoolsite/browser/views.py
+++ ageliaco/schoolsite/browser/views.py
@@ -1,5 +1,6 @@
 
 from datetime import date
+from datetime import datetime 
 
 #from zope import component
 
@@ -145,3 +146,128 @@ class WebmasterHelp(BrowserView):
                 return True
         return False
         
+
+
+
+
+
+def cleaning(mot):
+    # suppression des "
+    word = mot
+    if word[1]=='"':
+        word = word[1:]
+    if word[-1]=='"':
+        word = word[:-1]
+    return word
+
+def multisubject(mot):
+    # transforme une liste avec virgule en multiligne
+    sujets = mot.split(',')
+    word = ''
+    for s in sujets:
+        if word :
+            word += '\n' + s
+        else:
+            word = s
+    return word
+
+def getDateParts(datestring):
+    # date parts from a string of the form: 08/26/2013 07:30
+    first_split = datestring.split(' ')
+    date = first_split[0]
+    hour_and_min = first_split[1]
+
+    second_split = date.split('/')
+    month = second_split[0]
+    day = second_split[1]
+    year = second_split[2]
+
+    third_split = hour_and_min.split(':')
+    hour = third_split[0]
+    min = third_split[1]
+
+    return int(year), int(month), int(day), int(hour), int(min)
+
+
+
+class ImportEvents(BrowserView):
+    """
+    """
+
+    def __call__(self):
+
+        context = self.context
+        # recupere chaque ligne du fichier voulu (ici 'evenements')
+
+        events = context.evenements.data.split('\n')
+        nb_lines=len(events)
+
+        # liste tous les objets du dossier et creation d'une liste avec leur id
+
+        existing_objects = context.getFolderContents()
+        list_objects_id =[]
+        for existing_object in existing_objects:
+            this_object=existing_object.getObject()
+            this_object_id=this_object.getId()
+            list_objects_id.append(this_object_id)
+
+        # boucle sur tous les evenements du fichier CSV, on ne veut pas de la ligne 1 (labels) ni de la derniere (vide a cause du dernier '''\n''')
+
+        for event in events[1:nb_lines-1]:
+
+            # Parsing de chaque ligne ==> liste des attributs de l'objet evenement
+            event_fields= event.split(';')
+
+            # Supprimer element dont id identique a celui du fichier csv (si existe)
+            if event_fields[0] in list_objects_id:
+               context.manage_delObjects(event_fields[0])
+
+            # creation de l'evenement, on insere les proprietes de l'evenement selon la structure du fichier CSV de depart
+
+            event_id = event_fields[0]
+
+            event_start_datestr = event_fields[4]
+            event_start_dateparts = getDateParts(event_start_datestr)
+            event_start = datetime(event_start_dateparts[0],
+                                   event_start_dateparts[1],
+                                   event_start_dateparts[2],
+                                   event_start_dateparts[3],
+                                   event_start_dateparts[4])
+
+            event_end_datestr = event_fields[5]
+            event_end_dateparts = getDateParts(event_end_datestr)
+            event_end = datetime(event_end_dateparts[0],
+                                   event_end_dateparts[1],
+                                   event_end_dateparts[2],
+                                   event_end_dateparts[3],
+                                   event_end_dateparts[4])
+
+            #print "%s, %s" % (event_start_str, event_end_str)
+
+            context.invokeFactory(type_name="Event",
+                      id=event_id,
+                      title=event_fields[1],
+                      description=event_fields[2],
+                      location=event_fields[3],
+
+                      #startDate=event_fields[4],
+                      #endDate=event_fields[5],
+
+                      start=event_start,
+                      end=event_end,
+
+                      timezone="Europe/Vienna",
+
+                      text=event_fields[6],
+                      subject=multisubject(event_fields[7]),
+
+                     # effectiveDate=event_fields[8],
+                     # expirationDate=event_fields[9],
+                     # creation_date=event_fields[10],
+                     # modification_date=event_fields[11],
+                     # creators=event_fields[12]
+                      )
+            evt = context[event_id]
+            evt.reindexObject()
+ 
+        return "DONE"
